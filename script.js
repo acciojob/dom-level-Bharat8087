e3c1cba0b894f1962786f6887e8c8e74891fbf6f
Bharat8087 <157899792+Bharat8087@users.noreply.github.com>
2024-04-22 13:57:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+
+
+var targetElement = document.getElementById('level');
+var domLevel = 0;
+
+while (targetElement !== null && targetElement.nodeName !== 'HTML') {
+    targetElement = targetElement.parentNode;
+    domLevel++;
+}
+
+alert('The level of the element is: ' + domLevel);
